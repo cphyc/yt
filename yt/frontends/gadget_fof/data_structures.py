392f3d38f3bb1b92411b7458011306625b73f9ff
Corentin Cadiou <corentin.cadiou@cphyc.me>
2023-03-15 14:54:56 +0000

    Read parameters for Gadget FOF

--- yt/frontends/gadget_fof/data_structures.py
+++ yt/frontends/gadget_fof/data_structures.py
@@ -195,6 +195,10 @@ class GadgetFOFDataset(ParticleDataset):
             self.parameters = {
                 str(field): val for field, val in f["Header"].attrs.items()
             }
+            if "Parameters" in f:
+                self.parameters.update(
+                    {str(field): val for field, val in f["Parameters"].attrs.items()}
+                )
 
         self.dimensionality = 3
         self.refine_by = 2
